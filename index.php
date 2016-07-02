a85b7a88e62dc0337872d02aef24e1243867c0bf
Pasit Niyomthong <enqueteplu@gmail.com>
2016-07-02 23:28:21 +0700

    index and css begin

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Web Showrel</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <h1>hello</h1>
+  </body>
+</html>
